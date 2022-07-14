18ad30246182f2f7c04a5161da761816389c454f
CRSampaio <c.r.sampaio@outlook.com>
2022-07-14 04:49:44 -0300

    Add entities project

---
+++ src/FIGAudioServices.Entities/Files/Audio.cs
@@ -0,0 +1,17 @@
+﻿namespace FIGAudioServices.Entities.Files;
+public class Audio
+{
+    public int Id { get; set; }
+    public string Filename
+    {
+        get => Filename;
+        set
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(Filename));
+            }
+            Filename = value;
+        }
+    }
+}
---
+++ src/FIGAudioServices.Entities/Files/AudioAlert.cs
@@ -0,0 +1,19 @@
+namespace FIGAudioServices.Entities.Files;
+public class AudioAlert
+{
+    public int Id { get; set; }
+    public Audio Audio
+    {
+        get => Audio;
+        set
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(Audio));
+            }
+
+            Audio = value;
+        }
+    }
+    public string? Description { get; set; }
+}
