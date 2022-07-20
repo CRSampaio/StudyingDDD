8bc3a7b151f920bb508a87222ec9186acdf1d1fb
CRSampaio <c.r.sampaio@outlook.com>
2022-07-19 23:51:31 -0300

    Adjusted file namespace

--- src/FIGAudioServices.Infra/Interfaces/Base/IBasicRepository.cs
+++ src/FIGAudioServices.Infra/Interfaces/Base/IBasicRepository.cs
@@ -1,8 +1,7 @@
-﻿namespace FIGAudioServices.Infra.Interfaces;
+﻿namespace FIGAudioServices.Infra.Interfaces.Base;
 public interface IBasicRepository<T> where T : class
 {
     Task InsertAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
-
 }
