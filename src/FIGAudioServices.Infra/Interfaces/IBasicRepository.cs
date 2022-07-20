93fe3cd23ff254792ead3a36b1efc61e9e3d28b6
CRSampaio <c.r.sampaio@outlook.com>
2022-07-19 23:38:39 -0300

    Add base interface

---
+++ src/FIGAudioServices.Infra/Interfaces/IBasicRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace FIGAudioServices.Infra.Interfaces;
+public interface IBasicRepository<T> where T : class
+{
+    Task InsertAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+
+}
