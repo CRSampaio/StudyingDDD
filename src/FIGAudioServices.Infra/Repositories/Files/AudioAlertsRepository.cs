d1863b1238a7eab7fe7c7438c7b3f37d112ba187
CRSampaio <c.r.sampaio@outlook.com>
2022-08-08 23:57:57 -0300

    Implementing DbContext in repositories

--- src/FIGAudioServices.Infra/Repositories/Files/AudioAlertsRepository.cs
+++ src/FIGAudioServices.Infra/Repositories/Files/AudioAlertsRepository.cs
@@ -1,4 +1,5 @@
 using FIGAudioServices.Entities.Files;
+using FIGAudioServices.Infra.Context;
 using FIGAudioServices.Infra.Interfaces.Files;
 using FIGAudioServices.Infra.Repositories.Base;
 
@@ -6,6 +7,13 @@ namespace FIGAudioServices.Infra.Repositories.Files
 {
     public class AudioAlertsRepository : BasicRepository<AudioAlert>, IAudioAlertsRepository
     {
+        private readonly FIGAudioServicesDbContext Context;
+        
+        public AudioAlertsRepository(FIGAudioServicesDbContext context)
+        {
+            Context = context;
+        }
+
         public async Task<IList<AudioAlert>> GetAllAsync()
         {
             await Task.CompletedTask;
