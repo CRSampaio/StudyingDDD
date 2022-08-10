2aca14ec396064d37038b76a685fe67659ebe0c0
CRSampaio <c.r.sampaio@outlook.com>
2022-08-10 01:22:52 -0300

    Implemented interface methods

--- src/FIGAudioServices.Services/Services/Files/AudioAlertsService.cs
+++ src/FIGAudioServices.Services/Services/Files/AudioAlertsService.cs
@@ -1,7 +1,39 @@
+using FIGAudioServices.Entities.Files;
+using FIGAudioServices.Infra.Interfaces.Files;
+using FIGAudioServices.Services.Interfaces.Files;
+
 namespace FIGAudioServices.Services.Services.Files
 {
-    public class AudioAlertsService
+    public class AudioAlertsService : IAudioAlertsService
     {
-        
+        private readonly IAudioAlertsRepository AudioAlertsRepository;
+
+        private readonly IAudiosRepository AudiosRepository;
+
+        public AudioAlertsService(IAudioAlertsRepository audioAlertsRepository, IAudiosRepository audiosRepository)
+        {
+            AudioAlertsRepository = audioAlertsRepository;
+            AudiosRepository = audiosRepository;
+        }
+
+        public async Task CreateAudioAlertsAsync()
+        {
+            var audios = await AudiosRepository.GetAllAsync();
+
+            List<AudioAlert> audioAlerts = new List<AudioAlert>()
+            {
+                new AudioAlert(){Id = 1, Description = "Alerta do Primeiro"},
+                new AudioAlert(){Id = 2, Description = "Alerta do Segundo"},
+                new AudioAlert(){Id = 3, Description = "Alerta do Terceiro"},
+                new AudioAlert(){Id = 4, Description = "Alerta do Quarto"},
+                new AudioAlert(){Id = 5, Description = "Alerta do Quinto"}
+            };
+
+            foreach (var audioAlert in audioAlerts)
+            {
+                audioAlert.Audio = audios.FirstOrDefault(x => x.Id == audioAlert.Id)!;
+                await AudioAlertsRepository.InsertAsync(audioAlert);
+            }
+        }
     }
 }
--- src/FIGAudioServices.Services/Services/Files/AudiosService.cs
+++ src/FIGAudioServices.Services/Services/Files/AudiosService.cs
@@ -1,7 +1,33 @@
+using FIGAudioServices.Entities.Files;
+using FIGAudioServices.Infra.Interfaces.Files;
+using FIGAudioServices.Services.Interfaces.Files;
+
 namespace FIGAudioServices.Services.Services.Files
 {
-    public class AudiosService
+    public class AudiosService : IAudiosService
     {
-        
+        private readonly IAudiosRepository AudiosRepository;
+
+        public AudiosService(IAudiosRepository audiosRepository)
+        {
+            AudiosRepository = audiosRepository;
+        }
+
+        public async Task CreateAudiosAsync()
+        {
+            List<Audio> audios = new List<Audio>()
+            {
+                new Audio(){Id = 1, Filename = "Primeiro"},
+                new Audio(){Id = 2, Filename = "Segundo"},
+                new Audio(){Id = 3, Filename = "Terceiro"},
+                new Audio(){Id = 4, Filename = "Quarto"},
+                new Audio(){Id = 5, Filename = "Quinto"}
+            };
+
+            foreach (var audio in audios)
+            {
+                await AudiosRepository.InsertAsync(audio);
+            }
+        }
     }
 }
