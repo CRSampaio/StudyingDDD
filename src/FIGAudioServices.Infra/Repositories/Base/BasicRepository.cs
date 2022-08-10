1059302187b173579e0cafcc4a397a68d2875c77
CRSampaio <c.r.sampaio@outlook.com>
2022-08-09 23:31:16 -0300

    Modifyied BasicRepository implementation

--- src/FIGAudioServices.Infra/Repositories/Base/BasicRepository.cs
+++ src/FIGAudioServices.Infra/Repositories/Base/BasicRepository.cs
@@ -1,25 +1,33 @@
+using FIGAudioServices.Infra.Context;
 using FIGAudioServices.Infra.Interfaces.Base;
 
 namespace FIGAudioServices.Infra.Repositories.Base
 {
     public class BasicRepository<T> : IBasicRepository<T> where T : class
     {
+        protected readonly FIGAudioServicesDbContext Context;
+
+        public BasicRepository(FIGAudioServicesDbContext context)
+        {
+            Context = context;
+        }
+
         public async Task DeleteAsync(T entity)
         {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
+            Context.Remove(entity);
+            await Context.SaveChangesAsync();
         }
 
         public async Task InsertAsync(T entity)
         {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
+            Context.Add(entity);
+            await Context.SaveChangesAsync();
         }
 
         public async Task UpdateAsync(T entity)
         {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
+            Context.Update(entity);
+            await Context.SaveChangesAsync();
         }
     }
 }
